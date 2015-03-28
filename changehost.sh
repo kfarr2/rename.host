073dac8ad4d5231a48b7762456b67928cb2ac9bf
root <root@leadable.studdingsail>
2015-03-28 14:19:02 -0700

    Add logic & options

--- changehost.sh
+++ changehost.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-$again = True
-while [ $again ]
-    hostname=`shuf -n 2 /usr/share/dict/words | tr '\n' '.' | tr '[:upper:]' '[:lower:]' | tr -cd "[.a-z]" | sed "s/\.$//"`
-    hostname $hostname
-    sed -i "s/HOSTNAME=.*/HOSTNAME=$hostname/" /etc/sysconfig/network
-    clear
-    echo $hostname
-    read -p "Keep? [y/n]" again
-    if [ "$again" = "n" ]
-    then
-        $again = False
-    else
-        $again = True
+while true; do
+
+    new_name=`shuf -n 2 /usr/share/dict/words | tr '\n' '.' | tr '[:upper:]' '[:lower:]' | tr -cd "[.a-z]" | sed "s/\.$//"`
+    for i in `seq 1 5`;
+    do
+        echo
+    done
+    echo "New Hostname: $new_name"
+    echo
+    read -p "Keep? [ yes/no/cancel ] " keep
+    if [ "$keep" = "yes" ] || [ $keep = "y" ]; then
+        hostname=$new_name
+        hostname $hostname
+        sed -i "s/HOSTNAME=.*/HOSTNAME=$hostname/" /etc/sysconfig/network
+        echo "Restart session to view applied changes"
+        break
+    elif [ "$keep" = "cancel" ] || [ "$keep" = "c" ]; then
+        break
     fi
-echo "Exit session & login again to apply changes"
+done
